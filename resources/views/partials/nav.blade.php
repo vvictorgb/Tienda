6e44199601400f07144bc67637a2d48e59a9d4e0
victorIESLaVereda <vgomezbalaguer@gmail.com>
2025-02-05 21:10:08 +0100

    Coorecion nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,7 +5,7 @@
     <div style="justify-content: space-around;">
         <a href="{{ url('login') }}" style="color:black; text-decoration:none;">Inicio</a>
         <a href="{{ url('productos') }}" style="color:black; text-decoration:none;">Productos</a>
-        @if (auth()->user()->role === 'admin')
+        @if (auth()->check() && auth()->user()->role === 'admin')
             <a href="{{ url('crud') }}" style="color:black; text-decoration:none;">Admin</a>
         @endif
         <a href="" style="color:black; text-decoration:none;">Carrito</a>
